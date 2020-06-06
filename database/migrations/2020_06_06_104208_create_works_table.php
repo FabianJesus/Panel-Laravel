9983444150298561ba2e8435c10758b4585014e0
FabianJesus <fabian.jesus.jm@gmail.com>
2020-06-06 13:02:43 +0200

    migrations

---
+++ database/migrations/2020_06_06_104208_create_works_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('works', function (Blueprint $table) {
+            $table->id();
+            $table->string('direction');
+            $table->dateTime('start');
+            $table->dateTime('ending');
+            $table->double('budget');
+            $table->double('cost');
+            $table->unsignedBigInteger('client_id');
+            $table->foreign('client_id')->references('id')->on('clients');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('works');
+    }
+}
